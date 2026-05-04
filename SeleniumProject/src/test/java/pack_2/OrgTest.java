9a558a1e1f5c2b3fae8aa8f476edde23082cfce3
Dell <Dell@DESKTOP-HHQ3HI3>
2026-05-04 11:24:55 +0530

    updated project

---
+++ SeleniumProject/src/test/java/pack_2/OrgTest.java
@@ -0,0 +1,15 @@
+package pack_2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	@Test
+	public void createOrgTest() {
+		System.out.println("execute crateorgTest");
+	}
+
+	@Test
+	public void modifyorgTest() {
+		System.out.println("execute ModifyorgTest");
+	}
+}
